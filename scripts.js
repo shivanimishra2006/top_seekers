24a4d9daf167b860df29e506559a9df11d671253
shivanimishra2006 <shivani.mishra152006@gmail.com>
2026-06-05 00:48:31 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,35 +1,33 @@
-console.log("JS Loaded");
 let header = document.querySelector('header');
+
 window.addEventListener('scroll', () => {
-    header.classList.toggle('active',window.scrollY > 0);
+    header.classList.toggle('active', window.scrollY > 0);
 });
+
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("year").textContent = new Date().getFullYear();
 
     document.querySelector(".footer-newsletter button").addEventListener("click", function() {
         let email = document.querySelector(".footer-newsletter input").value;
 
-        if(email) {
+        if (email) {
             alert("Thank you for subscribing!");
         } else {
             alert("Please enter a valid email.");
         }
     });
 });
+
 function filterMenu(category) {
     const items = document.querySelectorAll('.Menu-box');
 
-    for(let i=0;i<items.length;i++)
-    {
-        const item=items[i];
+    for (let i = 0; i < items.length; i++) {
+        const item = items[i];
 
-        if(category==='all' || item.dataset.category===category)
-        {
-            item.style.display='block';
-        }
-        else
-        {
-            item.style.display='none';
+        if (category === 'all' || item.dataset.category === category) {
+            item.style.display = '';
+        } else {
+            item.style.display = 'none';
         }
     }
 }
